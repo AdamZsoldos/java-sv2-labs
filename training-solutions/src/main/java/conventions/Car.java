16308939fa8b24351731b354c088ee41d89027ca
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-18 17:55:46 +0200

    Updated conventions.Car and CarMain

--- training-solutions/src/main/java/conventions/Car.java
+++ training-solutions/src/main/java/conventions/Car.java
@@ -1,2 +1,53 @@
-package conventions;public class Car {
+package conventions;
+
+public class Car {
+
+    private String carType;
+    private String engineType;
+    private int doors;
+    private int persons;
+
+    public Car(String carType, String engineType, int doors, int persons) {
+        this.carType = carType;
+        this.engineType = engineType;
+        this.doors = doors;
+        this.persons = persons;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getPersons() {
+        return persons;
+    }
+
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+
+    public void addModelName(String modelName) {
+        carType = carType + " " + modelName;
+    }
+
 }
--- training-solutions/src/main/java/conventions/CarMain.java
+++ training-solutions/src/main/java/conventions/CarMain.java
@@ -1,2 +1,76 @@
-package conventions;public class CarMain {
+package conventions;
+
+import java.util.Scanner;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String carType = takeCarTypeInput(scanner);
+        String engineType = takeEngineTypeInput(scanner);
+        int doors = takeDoorsInput(scanner);
+        int persons = takePersonsInput(scanner);
+
+        Car car = new Car(carType, engineType, doors, persons);
+
+        boolean quit = false;
+
+        while (!quit) {
+            printCarInfo(car);
+
+            System.out.println("Enter command:");
+            System.out.println("Change [C] car type, [E] engine type, [D] number of doors or [P] passenger capacity");
+            System.out.println("[M] Add model name");
+            System.out.println("[Q] Quit");
+
+            String command = scanner.nextLine();
+
+            switch (command.toUpperCase()) {
+                case "C" -> car.setCarType(takeCarTypeInput(scanner));
+                case "E" -> car.setEngineType(takeEngineTypeInput(scanner));
+                case "D" -> car.setDoors(takeDoorsInput(scanner));
+                case "P" -> car.setPersons(takePersonsInput(scanner));
+                case "M" -> car.addModelName(takeModelInput(scanner));
+                case "Q" -> quit = true;
+            }
+        }
+    }
+
+    public static String takeCarTypeInput(Scanner scanner) {
+        System.out.println("Please enter car type:");
+        return scanner.nextLine();
+    }
+
+    public static String takeEngineTypeInput(Scanner scanner) {
+        System.out.println("Please enter engine type:");
+        return scanner.nextLine();
+    }
+
+    public static int takeDoorsInput(Scanner scanner) {
+        System.out.println("Please enter number of doors:");
+        int doors = scanner.nextInt();
+        scanner.nextLine();
+        return doors;
+    }
+
+    public static int takePersonsInput(Scanner scanner) {
+        System.out.println("Please enter passenger capacity:");
+        int persons = scanner.nextInt();
+        scanner.nextLine();
+        return persons;
+    }
+
+    public static String takeModelInput(Scanner scanner) {
+        System.out.println("Please enter model name:");
+        return scanner.nextLine();
+    }
+
+    public static void printCarInfo(Car car) {
+        System.out.println("Car type: " + car.getCarType());
+        System.out.println("Engine type: " + car.getEngineType());
+        System.out.println("Number of doors: " + car.getDoors() + " doors");
+        System.out.println("Passenger capacity: " + car.getPersons() + " persons");
+    }
 }
