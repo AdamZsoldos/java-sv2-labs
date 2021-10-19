6a72a99c1c5f3bf8963cdd7a0ece59140b03df7f
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-19 13:08:16 +0200

    Added linebreak.CityNames

---
+++ training-solutions/src/main/java/linebreak/CityNames.java
@@ -0,0 +1,13 @@
+package linebreak;
+
+public class CityNames {
+
+    public static void main(String[] args) {
+
+        String city1 = "Tokyo";
+        String city2 = "Delhi";
+        String city3 = "Shanghai";
+
+        System.out.print(city1 + System.lineSeparator() + city2 + System.lineSeparator() + city3);
+    }
+}
