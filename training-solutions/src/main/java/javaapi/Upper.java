8b477f3186e38eeea6a425eb494825f15f846cdd
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-19 09:26:55 +0200

    Added javaapi.Upper

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,13 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+        String s = "Hello World!";
+
+        System.out.println(s);
+        System.out.println(s.toUpperCase());
+    }
+
+}
