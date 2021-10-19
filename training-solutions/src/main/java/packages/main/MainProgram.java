172986ebc202dbd4f3af51a26052cdf289bad23b
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-19 03:54:43 +0200

    Added packages.greetings.Greeter and packages.main.MainProgram

---
+++ training-solutions/src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,11 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
+    }
+}
