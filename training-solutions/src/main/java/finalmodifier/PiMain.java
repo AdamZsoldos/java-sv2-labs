86e8e0b837164d00cbf4f225fcccc3dfd92e2c5d
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-21 17:21:30 +0200

    Updated finalmodifier.PiMain

--- training-solutions/src/main/java/finalmodifier/PiMain.java
+++ training-solutions/src/main/java/finalmodifier/PiMain.java
@@ -18,12 +18,16 @@ public class PiMain {
 
         System.out.println("Radius: " + r);
         System.out.println("Height: " + h);
+        System.out.println("Pi: " + CircleCalculator.PI);
+        System.out.println();
 
         System.out.println("Circle circumference: " + CircleCalculator.calculateCircumference(r));
         System.out.println("Circle area: " + CircleCalculator.calculateArea(r));
+        System.out.println();
 
         System.out.println("Cylinder surface area: " + CylinderCalculator.calculateSurfaceArea(r, h));
         System.out.println("Cylinder volume: " + CylinderCalculator.calculateVolume(r, h));
+        System.out.println();
 
         System.out.println("Cylinder surface area (based on circle): " + CylinderCalculatorBasedOnCircle.calculateSurfaceArea(r, h));
         System.out.println("Cylinder volume (based on circle): " + CylinderCalculatorBasedOnCircle.calculateVolume(r, h));
