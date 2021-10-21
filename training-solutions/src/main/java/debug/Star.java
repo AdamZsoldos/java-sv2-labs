6a3ff486d800b6a85bfc9f628fdc5f48a670d36d
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-21 14:28:25 +0200

    Added debug.Star

---
+++ training-solutions/src/main/java/debug/Star.java
@@ -0,0 +1,36 @@
+package debug;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Star {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[8];
+        Random random = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            int prev = i == 0 ? 0 : numbers[i - 1];
+            numbers[i] = random.nextInt(prev + 1, prev + 8);
+        }
+        System.out.println("numbers: " + Arrays.toString(numbers));
+        System.out.println();
+
+        writeStars(numbers);
+    }
+
+    public static void writeStars(int[] numbers) {
+        int prev = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int stars = numbers[i] - prev;
+            printStars(stars);
+            prev = numbers[i];
+            System.out.println();
+        }
+    }
+
+    private static void printStars(int stars) {
+        for (int i = 1; i <= stars; i++) {
+            System.out.print("*");
+        }
+    }
+}
