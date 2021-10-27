5ff75e04ef95ed19f95b59144f5ce0b451b8eb80
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-27 15:51:14 +0200

    Updated enumtype.university.University

--- training-solutions/src/main/java/enumtype/university/University.java
+++ training-solutions/src/main/java/enumtype/university/University.java
@@ -15,11 +15,3 @@ public enum University {
         return name;
     }
 }
-/*
-Gyakorlati feladat - Egyetemek
-
-A university csomagban hozz létre egy University nevű enumot, amelynek példányai néhány, általad tetszőlegesen
-választott magyarországi egyetem rövidített neve (pl. BME)! Legyen az enumnak egy String name attribútuma is,
-amelynek értékét a konstruktorában állítsa be: ez legyen minden példánynál az adott egyetem hivatalos neve,
-rövidítések nélkül kiírva. Teszteld a UniversityMain osztály main() metódusában!
- */
