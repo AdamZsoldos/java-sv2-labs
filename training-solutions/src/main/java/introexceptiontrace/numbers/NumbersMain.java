616ef480f9843285c8f40e90dcca3aef1a29d374
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-19 23:01:00 +0100

    Added introexceptiontrace.numbers.RandomOperations, Change and NumbersMain

---
+++ training-solutions/src/main/java/introexceptiontrace/numbers/NumbersMain.java
@@ -0,0 +1,10 @@
+package introexceptiontrace.numbers;
+
+import java.util.Arrays;
+
+public class NumbersMain {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Change().changeNumbers()));
+    }
+}
---
+++ training-solutions/src/main/java/introexceptiontrace/numbers/RandomOperations.java
@@ -0,0 +1,17 @@
+package introexceptiontrace.numbers;
+
+import java.util.Random;
+
+public class RandomOperations {
+
+    private final Random random = new Random();
+
+    public int[] getNumbers() {
+        int lengthOfArray = random.nextInt(10) + 1;
+        int[] numbers = new int[lengthOfArray];
+        for (int i = 0; i < lengthOfArray; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+        return numbers;
+    }
+}
