7e27a20083144db4f8bf4ae362f18f2fa9c2ca6b
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-19 14:49:37 +0100

    Added introexceptionmoreexceptions.Three

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Three.java
@@ -0,0 +1,19 @@
+package introexceptionmoreexceptions;
+
+import java.util.Arrays;
+
+public class Three {
+
+    public static void main(String[] args) {
+        int[] first = new int[]{3, 7, 24, 0, 466, 8, 0 ,42, 2, 6, 0};
+        int[] second = new int[]{676, 4, 31, 5, 2, 432, 643, 1, 1, 42, 456};
+        int[] third = new int[second.length];
+
+        for (int i = 0; i < second.length; i++) {
+            third[i] = first[i] / second[i];
+            System.out.println(third[i]);
+        }
+
+        System.out.println(Arrays.toString(third));
+    }
+}
