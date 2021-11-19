8ce375a67751d3918db434c39a1639db9176efa3
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-19 14:45:27 +0100

    Added introexceptionmoreexceptions.Person

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,35 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person(null, 32);
+        System.out.println(person);
+        System.out.println(person.getName());
+        System.out.println(person.getName().toUpperCase());
+    }
+}
