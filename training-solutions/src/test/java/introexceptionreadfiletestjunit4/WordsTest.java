1dea0b0f051a91ee945952420761e369067c8a1a
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-21 01:56:19 +0100

    Added introexceptionreadfiletestjunit4.Words and WordsTest

---
+++ training-solutions/src/test/java/introexceptionreadfiletestjunit4/WordsTest.java
@@ -0,0 +1,30 @@
+package introexceptionreadfiletestjunit4;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordsTest {
+
+    @Test
+    void testGetFirstWordWithA() {
+        String word = Words.getFirstWordWithA(Paths.get("src/test/resources/words.txt"));
+        assertEquals("Anna", word);
+    }
+
+    @Test
+    void testGetFirstWordWithANoA() {
+        String word = Words.getFirstWordWithA(Paths.get("src/test/resources/wordswithout.txt"));
+        assertEquals("A", word);
+    }
+
+    @Test
+    void testGetFirstWordWithAInvalidPath() {
+        Path path = Paths.get("src/test/resources/wordsnonexist.txt");
+        IllegalStateException ise = assertThrows(IllegalStateException.class, () -> Words.getFirstWordWithA(path));
+        assertEquals("Cannot read file", ise.getMessage());
+    }
+}
