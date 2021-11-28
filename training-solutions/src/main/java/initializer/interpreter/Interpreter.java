dae067cb53289e0597dd332a5de906c19001fae4
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-28 19:42:34 +0100

    Added initializer.interpreter.Interpreter

---
+++ training-solutions/src/main/java/initializer/interpreter/Interpreter.java
@@ -0,0 +1,40 @@
+package initializer.interpreter;
+
+public class Interpreter {
+
+    private String field;
+
+    public Interpreter() {
+        System.out.print(1);
+    }
+
+    {
+        System.out.print(2);
+    }
+
+    {
+        System.out.print(3);
+    }
+
+    static {
+        System.out.print(4);
+    }
+
+    public Interpreter(String field) {
+        System.out.print(5);
+    }
+
+    {
+        System.out.print(6);
+    }
+
+    static {
+        System.out.print(7);
+    }
+
+    public static void main(String[] args) {
+        Interpreter interpreterOne = new Interpreter();
+        Interpreter interpreterTwo = new Interpreter("");
+    }
+}
+// 4723612365
